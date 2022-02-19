343b3dcb6e84eeef20fd6bffc56df4bbad9519e7
Coursovra <isip_v.r.obuhov@mpt.ru>
2022-02-19 17:46:04 +0300

    AiPatrol

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Impingement.Combat;
+﻿using Impingement.Combat;
 using Impingement.Core;
 using Impingement.Movement;
 using Unity.Netcode;
@@ -10,51 +9,114 @@ namespace Impingement.Control
     public class AIController: NetworkBehaviour
     {
         [SerializeField] private float _chaseDistance = 5f;
+        [SerializeField] private float _suspicionTime =  5f;   
+        [SerializeField] private float _waypointDwellTime =  3f;   
+        [SerializeField] private float _waypointTolerance = 1;
+        [SerializeField] private PatrolPath _patrolPath;
         private CombatController _combatController;
         private HealthController _healthController;
-        [SerializeField] GameObject[] _players;
-        private PlayerController _target;
+        private MovementController _movementController;
+        private GameObject[] _players;
+        private Vector3 _guardPosition;
+        private float _timeSinceLastSawPlayer = Mathf.Infinity;
+        private float _timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        private int _currentWaypointIndex;
 
         private void Start()
         {
             _combatController = GetComponent<CombatController>();
             _healthController = GetComponent<HealthController>();
+            _movementController = GetComponent<MovementController>();
+            _guardPosition = transform.position;
         }
 
         private void Update()
         {
-            if (_healthController.IsDead())
-            {
-                _combatController.Cancel();
-                return;
-            }
-            
+            if (_healthController.IsDead()) { return; }
+
             var players = GameObject.FindGameObjectsWithTag("Player");
             if (players.Length == 0) { return; }
+            UpdateTimers();
 
             foreach (var player in players)
             {
                 if(player.GetComponent<HealthController>().IsDead()) { continue; }
-                var distance = Vector3.Distance(player.transform.position, transform.position);
-
-                if (distance <= _chaseDistance)
+                
+                if (InAttackRange(player) && _combatController.CanAttack(player))
                 {
-                    if (_combatController.CanAttack(player))
-                    {
-                        _combatController.SetTarget(player);
-                    }
-                }
-                else
-                {
-                    if (_combatController.GetTarget() == null) { continue;}
-                    if (Vector3.Distance(_combatController.GetTarget().transform.position, transform.position) >
-                        _chaseDistance || !_combatController.CanAttack(_combatController.GetTarget().gameObject))
-                    {
-                        _combatController.Cancel();
-                    }
+                    AttackBehaviour(player);
+                    return;
                 }
             }
+            
+            if(_timeSinceLastSawPlayer < _suspicionTime)
+            {
+                GetComponent<ActionScheduleController>().CancelCurrentAction();
+            }
+            else
+            {
+                PatrolBehaviour();
+            }
+        }
 
+        private void PatrolBehaviour()
+        {
+            Vector3 nextPosition = _guardPosition;
+            if (_patrolPath != null)
+            {
+                if (AtWaypoint())
+                {
+                    _timeSinceArrivedAtWaypoint = 0;
+                    CycleWaypoint();
+                }
+
+                nextPosition = GetCurrentWaypoint();
+            }
+
+            if (_timeSinceArrivedAtWaypoint > _waypointDwellTime)
+            {
+                _movementController.StartMoving(nextPosition);
+            }
+        }
+
+        private void AttackBehaviour(GameObject player)
+        {
+            _timeSinceLastSawPlayer = 0;
+            _combatController.SetTarget(player);
+        }
+
+        private void UpdateTimers()
+        {
+            _timeSinceLastSawPlayer += Time.deltaTime;
+            _timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return _patrolPath.GetWaypoint(_currentWaypointIndex);
+        }
+
+        private void CycleWaypoint()
+        {
+            _currentWaypointIndex = _patrolPath.GetNextIndex(_currentWaypointIndex);
+        }
+
+        private bool AtWaypoint()
+        {
+            float distance = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distance < _waypointTolerance;
+        }
+
+        private bool InAttackRange(GameObject player)
+        {
+            var distance = Vector3.Distance(transform.position, player.transform.position);
+            return distance < _chaseDistance;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, _chaseDistance);
         }
     }
 }
---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Impingement.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private const float _waypointGizmosRadius = 0.3f;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), _waypointGizmosRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        public int GetNextIndex(int i)
+        {
+            if (i + 1 >= transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        public Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
