ac82525bfa749099fb63079ef8036d0944810ef6
Coursovra <isip_v.r.obuhov@mpt.ru>
2022-05-11 11:26:41 +0300

    Inventory dragging

---
+++ Assets/Scripts/UI/Inventory/InventoryDragItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Impingement.UI.Dragging;
+
+namespace Impingement.UI.Inventory
+{
+    /// <summary>
+    /// To be placed on icons representing the item in a slot. Allows the item
+    /// to be dragged into other slots.
+    /// </summary>
+    public class InventoryDragItem : DragItem<Sprite>
+    {
+    }
+}
